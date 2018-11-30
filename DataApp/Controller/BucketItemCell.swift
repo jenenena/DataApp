95b6dee04847659ee593ff5a317224cc60ff1a36
Jenna Mills <superangelfish555@gmail.com>
2018-11-30 09:06:20 -0700

    Fixed issues with IBOutlets. Added randomEmoji function.

--- DataApp/Controller/BucketItemCell.swift
+++ DataApp/Controller/BucketItemCell.swift
@@ -5,23 +5,23 @@
 //  Created by Mills, Jenna on 11/30/18.
 //  Copyright © 2018 CTEC. All rights reserved.
 //
-import UIKit
-import Foundation
 
-public class BucketItem
-{
-    let itemContents : String
-    let itemAuthor : String
-    
-    init(contents : String, author : String)
-    {
-        itemContents = contents
-        itemAuthor = author
-    }
-}
+import UIKit
 
 class BucketItemCell: UITableViewCell {
 
+    @IBOutlet weak var BucketItemSymbol: UILabel!
+    @IBOutlet weak var BucketItemText: UILabel!
+    @IBOutlet weak var BucketItemSignature: UILabel!
+    
+    var currentBucketItem : BucketItem!
+    {
+        didSet
+        {
+            updateCellView()
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -31,6 +31,21 @@ class BucketItemCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    private func randomEmoji()->String
+    {
+        let emojiStart = 0x1F601
+        let emojiEnd = 0x1F64F
+        let symbolStart = 0x1F680
+        let symbolEnd = 0x1F6C5
+        
+        let emojiRange = 79
+        let symbolRange = 70
+        
+        let ascii = emojiStart + Int(arc4random_uniform(UInt32(emojiRange)))
+        let emoji = UnicodeScalar(ascii)?.description
+        return emoji!
     }
 
 }
